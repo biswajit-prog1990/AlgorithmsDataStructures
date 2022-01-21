c703b6076e9a1156553f7826215046045e566e57
biswajit-prog1990 <bisu.92.dutta@gmail.com>
2022-01-21 21:30:04 +0530

    commit

---
+++ AlgorithmsDataStructures/src/com/java/udemy/datastructures/deepdiveusingjava/SelectionSortImpl.java
@@ -0,0 +1,33 @@
+package com.java.udemy.datastructures.deepdiveusingjava;
+
+public class SelectionSortImpl {
+
+	public static void main(String[] args) {
+		int[] intArray = { 20, 35, -15, 7, 55, 1, -22 };
+
+		for (int lastUnsortedIndex = intArray.length - 1; lastUnsortedIndex > 0; lastUnsortedIndex--) {
+			int largest = 0;
+
+			for (int i = 1; i <= lastUnsortedIndex; i++) {
+				if (intArray[i] > intArray[largest])
+					largest = i;
+			}
+
+			swap(intArray, largest, lastUnsortedIndex);
+		}
+
+		for (int x : intArray) {
+			System.out.print(x + " ");
+		}
+	}
+
+	public static void swap(int[] intArray, int i, int j) {
+		if (i == j)
+			return;
+
+		int temp = intArray[i];
+		intArray[i] = intArray[j];
+		intArray[j] = temp;
+	}
+
+}
